62a6d3e6c71115165a19bed9d86042f65307575f
Kragen Javier Sitaker <kragen@canonical.org>
2011-09-21 11:34:33 -0300

    made hello1 look like hello0 again in the wake of import *

--- hello1.py
+++ hello1.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
-import pygame, random
+from pygame import *
+from random import randrange
 
-ww, hh = 1024, 600
-pantalla = pygame.display.set_mode((ww, hh), pygame.FULLSCREEN)
+pantalla = display.set_mode((0, 0), FULLSCREEN)
+ww, hh = pantalla.get_size()
 
-color = pygame.Color(64, 64, 192)
+color = Color(64, 64, 192)
 for xx in range(0, ww, 20):
     h, s, v, a = color.hsva
     color.hsva = xx * 270 / ww, s, v, a
-    pygame.draw.line(pantalla,
-                     color,
-                     (xx, 20),
-                     (random.randrange(ww), hh - 20),
-                     10)
-    pygame.display.flip()
+    draw.line(pantalla,
+              color,
+              (xx, 20),
+              (randrange(ww), hh - 20),
+              10)
+    display.flip()
