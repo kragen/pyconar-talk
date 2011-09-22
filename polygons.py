e361932a018c329f19a4d8cbcad29b1a723aa181
Kragen Javier Sitaker <kragen@canonical.org>
2011-09-22 11:40:18 -0300

    cleaned up polygons example a bit more

--- polygons.py
+++ polygons.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
 # basado en
 # http://lists.canonical.org/pipermail/kragen-hacks/2008-January/000477.html
+# lo cual es inspirado por Pedal (1995), por Dave Moore
 from pygame import *
 from math import pi, sin, cos
 
 pantalla = display.set_mode((0, 0), FULLSCREEN)
 ww, hh = pantalla.get_size()
-color, d_d_theta, r = (255, 64, 64), pi/40, hh/2
-d_theta = 0
+theta, d_theta, r, color = 0, 0.04, hh/2, (255, 64, 64)
 
 while event.poll().type not in (MOUSEBUTTONDOWN, QUIT):
-    d_theta += d_d_theta
+    theta += d_theta
 
-    puntos = [(ww/2 + sin(ii * d_theta) * r,
-               hh/2 - cos(ii * d_theta) * r)
-              for ii in range(3 + d_theta // (2 * pi))]
+    puntos = [(ww/2 + sin(ii * theta) * r, # x
+               hh/2 - cos(ii * theta) * r) # y
+              for ii in range(3 + theta // (2 * pi))]
 
-    pantalla.fill(0)
+    pantalla.fill(0)                    # llenar con negro
     draw.polygon(pantalla, color, puntos)
 
     display.flip()
