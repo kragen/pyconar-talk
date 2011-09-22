70d78b6fcd385bb49efd59a12ec1bb95c837e3bc
Kragen Javier Sitaker <kragen@canonical.org>
2011-09-22 02:28:53 -0300

    commented hello0.py

--- hello0.py
+++ hello0.py
@@ -6,9 +6,9 @@ pantalla = display.set_mode((0, 0), FULLSCREEN)
 ww, hh = pantalla.get_size()
 
 for xx in range(0, ww, 20):
-    draw.line(pantalla,
-              (64, 64, 192),
-              (xx, 20),
-              (randrange(ww), hh - 20),
-              10)
+    draw.line(pantalla,                 # surface
+              (64, 64, 192),            # color
+              (xx, 20),                 # punto de inicio
+              (randrange(ww), hh - 20), # punto de terminar
+              10)                       # ancho de rayo
     display.flip()
