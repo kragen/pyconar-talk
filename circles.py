7067d287fdd9ec94ee090d42867b0fbb69228cb3
kragen <kragen@VOSTRO9.(none)>
2011-09-22 14:32:51 -0300

    made circles move and fade in

--- circles.py
+++ circles.py
@@ -3,18 +3,21 @@ from pygame import *
 from random import randrange as rand
 
 pantalla = display.set_mode((0, 0), FULLSCREEN)
+ww, hh = pantalla.get_size()
 
 circles = []
 for ii in range(20):
     ss = rand(500)
     surface = Surface((ss, ss), SRCALPHA)
     draw.ellipse(surface,
-                 Color(rand(256), rand(256), rand(256), 128),
+                 Color(rand(256), rand(256), rand(256), 32),
                  surface.get_rect())
-    circles.append(surface)
+    circles.append((surface, { 'x': rand(ww),  'y': rand(hh),
+                              'dx': rand(10), 'dy': rand(10)}))
 
 while event.poll().type not in (QUIT, MOUSEBUTTONDOWN):
-    pantalla.blit(circles[rand(len(circles))],
-                  (rand(pantalla.get_width()),   # x
-                   rand(pantalla.get_height()))) # y
+    for surface, estado in circles:
+        pantalla.blit(surface, (estado['x'], estado['y']))
+        estado['x'] = (estado['x'] + estado['dx']) % ww
+        estado['y'] = (estado['y'] + estado['dy']) % hh
     display.flip()
